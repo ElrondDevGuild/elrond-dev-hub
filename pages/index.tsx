89ab8809a468fa13a37ab0295c89dce8d9039383
Razvan Statescu <razvan@statescu.net>
2022-06-20 16:10:37 +0300

    implement post card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
-import Layout from '../components/Layout';
-import PostItem from '../components/PostItem';
+import { NextPage } from 'next';
 
-import type { NextPage } from "next";
+import Layout from '../components/Layout';
+import PostItem, { IPostItem } from '../components/PostItem';
+
+const dummyPost: IPostItem = {
+  title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, suscipit.",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, laboriosam. Odio et obcaecati, temporibus adipisci quasi tenetur cumque atque beatae!",
+  image: "https://picsum.photos/682/350",
+  category: "Category1",
+  author: "John Doe",
+  url: "https://elrondgiants.com",
+};
 const Home: NextPage = () => {
   return (
     <Layout>
-      <PostItem />
+      <PostItem post={dummyPost} />
     </Layout>
   );
 };
