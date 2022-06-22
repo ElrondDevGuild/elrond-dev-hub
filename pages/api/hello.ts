f5e9d592d2e0fb8602edd666d8b99b41e1a6e374
Razvan Statescu <razvan@statescu.net>
2022-06-22 15:04:26 +0300

    install og-image & jimp

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const ogs = require("open-graph-scraper");
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+const getOgImage = (url: string): Promise<string> => {
+  return ogs({ url }).then((data: any) => {
+    const { error, result, response } = data;
+    return result?.ogImage?.url;
+  });
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ name: "John Doe" });
 }
